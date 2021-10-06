c34c75db499ddcbdfa567154a4724ae69934ac55
Mykola Zhabko <kokozhansei@gmail.com>
2021-10-06 16:33:56 +0300

    animation ready basic

--- src/com/mz/bear/sprites/AnimatedSprite.java
+++ src/com/mz/bear/sprites/AnimatedSprite.java
@@ -1,12 +1,17 @@
 package com.mz.bear.sprites;
 
 public class AnimatedSprite extends Sprite{
+
     public static final int TOTAL_MOVEMENTS = 2;
     public static final int RIGHT = 0;
     public static final int LEFT = 1;
 
+    public static final byte SPRITE_CHANGE = 10;
+
+
     protected int currentDirection;
     protected byte currentSprite;
+    protected byte currentSpriteChange;
 
     protected int[][] spriteXCoordinates = new int[TOTAL_MOVEMENTS][];
     protected int[][] spriteYCoordinates = new int[TOTAL_MOVEMENTS][];
@@ -16,14 +21,20 @@ public class AnimatedSprite extends Sprite{
         super(width, height);
         currentDirection = RIGHT;
         currentSprite = 0;
+        currentSpriteChange = 0;
     }
 
     public void animate(int movement){
         if (movement != currentDirection){
             currentDirection = movement;
             currentSprite = 0;
+            currentSpriteChange = 0;
         } else {
-            currentSprite = (byte)((currentSprite + 1) % spriteXCoordinates[currentDirection].length);
+            currentSpriteChange++;
+            if (currentSpriteChange > SPRITE_CHANGE) {
+                currentSprite = (byte) ((currentSprite + 1) % spriteXCoordinates[currentDirection].length);
+                currentSpriteChange = 0;
+            }
         }
 
         updateSpriteCoordinates();
--- src/com/mz/bear/sprites/Player.java
+++ src/com/mz/bear/sprites/Player.java
@@ -2,7 +2,7 @@ package com.mz.bear.sprites;
 
 import javafx.scene.image.Image;
 
-public class Player extends Sprite{
+public class Player extends AnimatedSprite{
 
     public static final int PLAYER_WIDTH = 96;
     public static final int PLAYER_HEIGHT = 96;
@@ -15,8 +15,12 @@ public class Player extends Sprite{
     public Player() {
         super(PLAYER_WIDTH, PLAYER_HEIGHT);
         spriteImage = new Image(IMAGE_PATH);
-        spriteX = 0;
-        spriteY = 0;
+
+        spriteXCoordinates[RIGHT] = new int[]{480, 576, 672, 576};
+        spriteYCoordinates[RIGHT] = new int[]{0, 0, 0, 0};
+        spriteXCoordinates[LEFT] = new int[]{1248, 1344, 1440, 1344};
+        spriteYCoordinates[LEFT] = new int[]{0, 0, 0, 0};
+
     }
 
     public void move(int movement){
@@ -27,5 +31,6 @@ public class Player extends Sprite{
             newX += 1;
         }
         moveTo(newX, y);
+        animate(movement);
     }
 }
